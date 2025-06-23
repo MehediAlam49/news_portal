7f82328e2f7c1722b29ea1a202d69390818fc2ec
MehediAlam49 <mehedialam806@gmail.com>
2025-06-23 19:11:35 +0600

    add html in html file

--- news_portal/newsApp/views.py
+++ news_portal/newsApp/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,HttpResponse
 
 # Create your views here.
+# --------------Authentication-------------------->
+def registerPage(request):
+    return render(request, 'registerPage.html')
+def loginPage(request):
+    return render(request, 'loginPage.html')
+def logoutPage(request):
+    return redirect('home')
+
+# ---------------CRUD--------------------------->
+def home(request):
+    return render(request, 'home.html')
+def addNews(request):
+    return render(request, 'addNews.html')
+def editNews(request):
+    return render(request, 'editNews.html')
+def viewNews(request):
+    return render(request, 'viewNews.html')
+def deleteNews(request):
+    return render(request, 'deleteNews.html')
